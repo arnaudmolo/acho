cda81b30a844e48b6916062ebf3b17d64a3c5182
Arnaud Pichon <arnaud@onesime.fr>
2017-10-10 19:50:09 +0200

    go to preload

--- src/Projects.js
+++ src/Projects.js
@@ -1,21 +1,71 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Prismic from 'prismic-javascript'
 import cx from 'classnames'
+import GSComponent from './GSComponent'
 
 import './Projects.css'
 // import Project from './Project'
 
-class Project extends React.Component {
+class SlideInOut extends GSComponent {
+  componentDidMount () {
+    this.timeline
+      .fromTo(this.$el, 0.66, {
+        yPercent: -100
+      }, {
+        yPercent: 0
+      })
+  }
+  render () {
+    return (
+      <div className='slide__container'>
+        <div className='slide__content' ref={e => { this.$el = e }}>{this.props.children}</div>
+      </div>
+    )
+  }
+}
+
+class Cover extends GSComponent {
+  componentDidMount () {
+    console.log(this.props)
+    if (this.props.visible) {
+      this.fadeIn(this.props)
+    }
+  }
+  fadeOut (props) {
+
+  }
+  fadeIn (props) {
+    this.timeline.fromTo(this.$cover, 1, {
+      alpha: 0,
+      height: 0
+    }, {
+      alpha: 1,
+      height: 280
+    })
+  }
   render (props = this.props) {
-    console.log(props.data)
+    return (
+      <div ref={e => { this.$cover = e }} className='project__cover'>
+        <img alt='cover' className='project__image' src={props.src} />
+      </div>
+    )
+  }
+}
+
+class Project extends GSComponent {
+  render (props = this.props) {
     const title = props.data.title[0].text
     return (
-      <div className='project'>
+      <div ref={e => { this.$project = e }} className='project'>
         <div className='project__titraille'>
-          <p className='project__chapo'>project</p>
-          <h1 className='project__title'>{title}</h1>
-          {props.data.services.length > 1 ?
-            <div>
+          <SlideInOut>
+            <p className='project__chapo'>project</p>
+          </SlideInOut>
+          <SlideInOut>
+            <h1 ref={e => { this.$title = e }} className='project__title'>{title}</h1>
+          </SlideInOut>
+          {props.data.services.length > 1
+            ? <div>
               <h4>Services</h4>
               <ul>
                 {props.data.services.map(({service}) =>
@@ -25,31 +75,28 @@ class Project extends React.Component {
             </div>
           : null}
         </div>
-        <div className='project__cover'>
-          <img alt={`${title} cover`} className='project__image' src={props.data.cover.url} />
-        </div>
+        <Cover src={props.data.cover.url} visible={props.showCover} />
       </div>
     )
   }
 }
 
-class Projects extends Component {
-  state = {
-    projects: []
+class Projects extends GSComponent {
+  constructor (props) {
+    super(props)
+    this.state = {
+      projects: []
+    }
   }
+
   componentDidMount () {
     this.load(this.props)
-    this.animate(this.props)
-  }
-
-  animate (props) {
-
   }
 
   load (props) {
     props.api.query(
       Prismic.Predicates.at('document.type', 'project')
-    ).then( response => {
+    ).then(response => {
       this.setState({
         projects: response.results
       })
@@ -62,10 +109,10 @@ class Projects extends Component {
       <div className='projects'>
         {this.state.projects.map((project, index) =>
           <div key={project.id} className={cx({
-              'projects-project': true,
-              'projects-project__selected': selected === index
-            })}>
-            <Project data={project.data} />
+            'projects-project': true,
+            'projects-project__selected': selected === index
+          })}>
+            <Project data={project.data} showCover={selected === index} />
           </div>
         )}
       </div>
