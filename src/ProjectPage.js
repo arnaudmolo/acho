aa2ce8e3b9899678e56619eebe0f59ecf7df14fa
Arnaud Pichon <arnaud@onesime.fr>
2017-10-15 21:04:26 +0200

    horizontal scroll

--- src/ProjectPage.js
+++ src/ProjectPage.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './ProjectPage.css'
 import { oneProject } from './redux/projects'
+import arrow from './arrow-right.svg'
 
 const toSimpleProject = project => ({
   title: project.data.title[0].text,
   cover: project.data.cover.url,
   year: project.data.year,
-  services: project.data.services.map(({service}) =>
+  services: project.data.services[0].service.length >= 1 ? project.data.services.map(({service}) =>
     service[0].text
-  )
+  ) : []
 })
 
 const Services = props =>
@@ -21,14 +22,29 @@ const Services = props =>
     </p>
   </div>
 
+const CTA = props =>
+  <div className='page--cta'>
+    <img className='page--arrow' alt='arrow' src={arrow} />
+  </div>
+
 class ProjectPage extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleResize = this.handleResize.bind(this)
+  }
+  componentDidMount () {
+    this.$container.addEventListener('scroll', this.handleResize)
+  }
+  handleResize (e) {
+    console.log(e)
+  }
   render (props = this.props) {
     if (!props.project) {
       return null
     }
     const project = toSimpleProject(props.project)
     return (
-      <div className='page--container'>
+      <div className='page--container' ref={e => { this.$container = e }}>
         <div className='page'>
           <div className='box'>
             <div className='box--content'>
@@ -36,8 +52,9 @@ class ProjectPage extends React.Component {
                 <p className='page--project'>Project</p>
                 <h2 className='page--title'>{project.title}</h2>
                 {project.services.length >= 1 && <Services services={project.services} />}
+                <CTA />
               </div>
-              <img alt='cover' src={project.cover} />
+              <div className='page--cover-container'><img alt='cover' src={project.cover} /></div>
             </div>
           </div>
           <div className='box' />
@@ -49,4 +66,4 @@ class ProjectPage extends React.Component {
   }
 }
 
-export default oneProject(ProjectPage)
+export default oneProject(props => props.project ? <ProjectPage {...props} /> : <div />)
